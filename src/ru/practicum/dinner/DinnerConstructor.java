c6b73f3ee17630424ef3026abf29b155e78adc40
Aleksandr Ivanov <sasha942710@yandex.ru>
2024-04-07 20:11:55 +0300

    Оптимизировал приложение. - Добавил новый метод подсказки. - Сделал, чтобы при вводе типа и названия блюда, не зависимо от регистра, записывалось в единый тип. Пример "Первое" - "первое" - будет как единый тип блюда - "первое". - Запретил доступ к команде 2, пока не будет добавлено хотя бы 1 тип в меню.

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -1,5 +1,68 @@
+/**
+ * Данный класс хранит данные типов блюда и соответствующие типу, списки блюда.
+ * Также в данном классе имеются методы:
+ * 1. Проверка по типу блюда - checkType()
+ * <p>
+ * 2. Добавление нового типа блюд и внесение блюда в данный тип. Если такой тип блюдо есть,
+ * то он дополняет список к данному типу блюд. - addTypeAndNameDish()
+ * <p>
+ * 3. Генерирует комбинацию блюд в зависимости от веденных данных. - comboDish()
+ * <p>
+ * 4. Добавил еще рандомную подсказку, которая помогает при выборе типа для набора.
+ */
 package ru.practicum.dinner;
 
-public class DinnerConstructor {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
+public class DinnerConstructor {
+    HashMap<String, ArrayList<String>> menu = new HashMap<>();
+    Random random = new Random();
+
+    boolean checkType(String type) {
+
+        return menu.containsKey(type);
+    }
+
+    void addTypeAndNameDish(String dishType, String dishName) {
+
+        ArrayList<String> listOfDish = new ArrayList<>();
+        if (checkType(dishType)) {  // проверяем есть ли такой тип блюда
+            listOfDish.add(dishName);
+            menu.get(dishType).add(dishName);
+            System.out.println("К типу блюд - " + dishType.toLowerCase() + ". \nДобавлено еще одно блюдо - "
+                    + dishName.toLowerCase());
+        } else {  //  Если такого типа нет. Добавляем новый тип блюда и добавляем туда само блюдо
+            listOfDish.add(dishName);
+            menu.put(dishType, listOfDish);
+            System.out.println("Вы добавили новый тип блюда - " + dishType.toLowerCase() + ". \nБлюдо - "
+                    + dishName.toLowerCase() + ", внесен в данный тип");
+        }
+    }
+
+    void comboDish(int numberOfCombos, ArrayList<String> typesOfDish) { // Делаем комбо набор
+
+        for (int i = 0; i < numberOfCombos; i++) {
+            System.out.println("Комбо " + (i + 1));
+            ArrayList<String> comboPrint = new ArrayList<>();
+
+            for (String ofDish : typesOfDish) {
+                ArrayList<String> combo = menu.get(ofDish); // Заносим все блюда и выбранных типов.
+                String randomDish = combo.get(random.nextInt(combo.size()));
+                comboPrint.add(randomDish);
+            }
+
+            System.out.println(comboPrint + "\n");
+        }
+    }
+
+    String availableTypesOfDishes() {//Метод для вывода подсказки ввода. Если админ забыл какой тип блюда можно выбрать.
+
+        String availableTypes;
+        ArrayList<String> randomType = new ArrayList<>(menu.keySet());
+        availableTypes = randomType.get(random.nextInt(randomType.size()));
+        return availableTypes;
+    }
 }
+
--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -1,23 +1,28 @@
+/**
+ * 1. У приложения будет два основных сценария использования. Работа с ним должна происходить так:
+ * Сначала администрация ресторана добавляет в программу доступные для заказа блюда.
+ * Для этого пользователь вводит тип блюда (например, Первое, Второе, Напиток) и само название.
+ * Допустим, он может добавить блюдо Солянка мясная с типом Второе.
+ * <p>
+ * 2. Когда доступные блюда сохранены, администрация ресторана переходит к генерации комбинаций для бизнес-ланча.
+ * Для этого пользователь указывает набор типов блюд, которые должны входить в бизнес-ланч.
+ * Например, Первое, Второе и Напиток.
+ * В ответ программа выводит одну или несколько возможных комбинаций из блюд этих типов.
+ * После одобрения администрацией ресторан сможет использовать получившиеся комбинации в меню бизнес-ланчей.
+ */
+
 package ru.practicum.dinner;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
     static DinnerConstructor dc;
     static Scanner scanner;
-    static Random random;
-    static HashMap<String, ArrayList<String>> menu = new HashMap<>();
-
 
     public static void main(String[] args) {
         dc = new DinnerConstructor();
         scanner = new Scanner(System.in);
-        random = new Random();
         boolean flag = true;
 
 
@@ -37,93 +42,63 @@ public class Main {
     }
 
     private static void printMenu() {
+
         System.out.println("Выберите команду:");
         System.out.println("1 - Добавить новое блюдо");
         System.out.println("2 - Сгенерировать комбинации блюд");
         System.out.println("3 - Выход");
     }
 
-    /**
-     * 1 — добавление нового блюда. Программа поочерёдно запрашивает у пользователя тип и название блюда и сохраняет их.
-     * Пользователь может ввести любую строку в качестве типа или названия.
-     * <p>
-     * Пример: в качестве типа блюда пользователь ввёл Напиток, а в качестве названия — Сок яблочный.
-     */
-    private static void addNewDish() {
-        System.out.println("Введите тип блюда:");
-        String dishType = scanner.nextLine();
-        System.out.println("Введите название блюда:");
-        String dishName = scanner.nextLine();
+    private static void addNewDish() { // добавляем тип блюда и само блюдо в меню.
 
-        // добавьте новое блюдо
-        ArrayList<String> listOfDish = new ArrayList<>();
-        if (!menu.isEmpty()) {  // если список не пуст
-            if (menu.containsKey(dishType)) {  // проверяем есть тмкой тип блюда
-                listOfDish.add(dishName);  //Если есть, вносим блюдо в список
-                menu.get(dishType).add(dishName); // и записываем его в тип
-            } else {
-                listOfDish.add(dishName);  //  если  такого типа нет.  , добавляем блюдо в список
-                menu.put(dishType, listOfDish);  // и создаем новый тип и добавляем туда блюдо
-                System.out.println(menu);
-            }
-        } else {
-            listOfDish.add(dishName);  // если список пустой, добавляем блюдо в список
-            menu.put(dishType, listOfDish);  // создаем тип и добавиляем в него блюдо
-            System.out.println(menu);
-        }
+        System.out.println("Введите тип блюда:");
+        String dishType = scanner.nextLine().toLowerCase();
+        System.out.println("Введите название блюда:");
+        String dishName = scanner.nextLine().toLowerCase();
+
+        dc.addTypeAndNameDish(dishType, dishName);
     }
 
-    /**
-     * 2
-     * 2 — генерация комбинаций блюд. Приложение запрашивает у пользователя количество комбинаций,
-     * а затем список типов блюд, которые должны входить в эти комбинации.
-     * Программа генерирует несколько вариантов для одного и того же набора типов блюд. Условия:
-     * <p>
-     * каждый тип вводится с новой строки;
-     * комбинации должны быть составлены из существующих блюд этих типов случайным образом;
-     * количество типов блюд в комбинации не ограничено, при этом типы могут повторяться;
-     * для завершения ввода пользователь должен ещё раз нажать Enter;
-     * если пользователь ввёл несуществующий тип, программа должна вывести предупреждающее сообщение и предложить ввести другой тип.
-     * <p>
-     * Пример: пользователь запросил две комбинации для набора Напиток, Гарнир, Мясо. Тогда программа может вывести следующее:
-     * Морс облепиховый, Гречка, Котлета пожарская и Сок яблочный, Картофельное пюре, Поджарка мясная.
-     */
 
-    private static void generateDishCombo() {
-        System.out.println("Начинаем конструировать обед...");
+    private static void generateDishCombo() {  // Генератор для комбо
+        if (!dc.menu.isEmpty()) { // Пока в меню ничего нет, доступ к комбинации будет закрыт.
+            System.out.println("Начинаем конструировать обед...");
 
-        System.out.println("Введите количество наборов, которые нужно сгенерировать:");
-        int numberOfCombos = scanner.nextInt();
-        scanner.nextLine();
+            System.out.println("Введите количество наборов, которые нужно сгенерировать:");
+            int numberOfCombos = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). Для завершения ввода введите пустую строку");
+            System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter)."
+                    + " Для завершения ввода введите пустую строку");
 
+        /*
+          Комментарий для Давида.
+          То что ниже код do while, я бы тоже запихнул в DinnerConstructor.
+          Но в ТЗ указано, что сканер должен остаться в main и не фигурировать в другом классе =)
+         */
 
-        //реализуйте ввод типов блюд
-        ArrayList<String> typesOfDish = new ArrayList<>(); // создал список для типа блюд
+            ArrayList<String> typesOfDish = new ArrayList<>(); /* создал список для типа блюд, которые будут комбинировать
+                                                              с комбо. */
+            String nextItem;
+            do {
+                nextItem = scanner.nextLine().toLowerCase();
+                if (!nextItem.isEmpty()) { // Проверка на пустую строку
 
-        String nextItem;
-        do {
-            nextItem = scanner.nextLine();
-            if (!nextItem.isEmpty()) { // если введенной тип не пустая строка
-                if (menu.containsKey(nextItem)) { // првоерка по типу блюд, если есть, добавляем
-                    typesOfDish.add(nextItem);
-                } else {
-                    System.out.println("Такого типа блюд у нас нет. Введите другой тип.");
+                    if (dc.checkType(nextItem)) { // проверка по типу блюд, если есть такой тип блюда, добавляем.
+                        typesOfDish.add(nextItem);
+                        System.out.println("Данный тип блюда у нас есть, тип блюда " + nextItem.toLowerCase()
+                                + " добавлен для подбора комбо.");
+                        System.out.println("Напоминаем! Для завершения ввода, оставьте пустую строку и нажмите(Enter)");
+                    } else {
+                        System.out.println("Такого типа блюд у нас нет. Введите другой тип.");
+                        System.out.println("Например: тип блюда - " + dc.availableTypesOfDishes());
+                    }
                 }
-            }
-        } while (!nextItem.isEmpty()); // если поле пустое, то выходим из цикла
+            } while (!nextItem.isEmpty()); // если поле пустое, то выходим из цикла
 
-
-        // сгенерируйте комбинации блюд и выведите на экран
-        for (int i = 0; i < numberOfCombos; i++) {
-            System.out.println("Комбо " + (i + 1));
-            for (String ofDish : typesOfDish) { // тут все блюда у нас есть.проверка на то что такого блюда нет, проводить не надо
-                ArrayList<String> combo = menu.get(ofDish); // сюда буду заносить, все блюда из указанного типа.
-                String randomDish = combo.get(random.nextInt(combo.size()));
-                System.out.print(randomDish + " ");
-            }
-            System.out.println();
+            dc.comboDish(numberOfCombos, typesOfDish); // Генерируем комбинацию блюд.
+        } else {
+            System.out.println("Для работы с этим пунктом, нужно заполнить меню через команду 1." + "\n");
         }
     }
 }
